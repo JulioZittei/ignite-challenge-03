1dd79b79f80dbf67649fea5a646895e1551470e3
Julio Zittei <julio.zittei96@gmail.com>
2021-11-20 22:16:39 -0300

    Add miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,44 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import response from './assets/server.json';
+
+createServer({
+  models: {
+    product: Model,
+    stock: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      stock: response.stock,
+      products: response.products,
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/stock', () => {
+      return this.db.stock;
+    });
+
+    this.get('/stock/:id', (_, request) => {
+      const { id } = request.params;
+      return this.db.stock.findBy({ id: id });
+    });
+
+    this.get('/products', () => {
+      return this.db.products;
+    });
+
+    this.get('/products/:id', (_, request) => {
+      const { id } = request.params;
+      return this.db.products.findBy({ id: id });
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const env =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://rocketshoes-julio-zittei.netlify.app/api';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `${env}`,
 });
