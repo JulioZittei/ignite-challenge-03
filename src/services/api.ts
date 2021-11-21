56f171a762136b73c8a4a3a66d1e9c4080a43ce7
Julio Zittei <julio.zittei96@gmail.com>
2021-11-21 10:57:53 -0300

    Update baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const env =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/api'
-    : 'https://rocketshoes-julio-zittei.netlify.app/api';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://ignite-challenge-03.netlify.app/api';
 
 export const api = axios.create({
   baseURL: `${env}`,
